5b03ffe752127cf9836d0248d3cfacff4846eac7
rinitsuha419 <s1f102400639@iniad.org>
2024-05-03 17:53:12 +0900

    20240503 17:52

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,8 @@ const Home = () => {
   const [turnColor, setTrunColor] = useState(1);
   const [board, setBoard] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 2, 0, 0, 0],
-    [0, 0, 0, 0, 2, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 2, 0, 0, 0],
     [0, 0, 0, 1, 2, 0, 0, 0],
     [0, 0, 0, 2, 1, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
@@ -18,12 +18,12 @@ const Home = () => {
     console.log(x, y);
     const newBoard = structuredClone(board);
 
-    for (let i = 0; i < 3; i++) {
-      if (board[y + 2][x] === turnColor) {
+    for (let i: number = 0; i < 3; i++) {
+      if (board[y + i + 2][x] === turnColor) {
         newBoard[y][x] = turnColor;
       }
     }
-
+    console.log(turnColor);
     if (board[y][x + 1] === 3 - turnColor && board[y][x + 2] === turnColor) {
       newBoard[y][x] = turnColor;
       newBoard[y][x + 1] = turnColor;
